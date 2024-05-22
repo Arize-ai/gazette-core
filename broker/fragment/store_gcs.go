1fd4cd676dfb624328fe1507da87e286836aeabb
Darren Dowker <darren@arize.com>
2024-05-22 10:41:53 -0700

    fix map keys

--- broker/fragment/store_gcs.go
+++ broker/fragment/store_gcs.go
@@ -37,7 +37,7 @@ func (s *gcsBackend) Provider() string {
 }
 
 func (s *gcsBackend) SignGet(ep *url.URL, fragment pb.Fragment, d time.Duration) (string, error) {
-	log.WithFields(log.Fields{ep: ep.String(), fragment: fragment.ContentPath()}).Info("*** DJD SignGet() called")
+	log.WithFields(log.Fields{"ep": ep.String(), "fragment": fragment.ContentPath()}).Info("*** DJD SignGet() called")
 	return "", nil
 	/*
 		cfg, client, opts, err := s.gcsClient(ep)
