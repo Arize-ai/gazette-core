57846ad65605b758e5f22f85db3e1a0829e510b7
Darren Dowker <darren@arize.com>
2024-05-22 14:04:22 -0700

    remove logs

--- broker/fragment/store_gcs.go
+++ broker/fragment/store_gcs.go
@@ -41,8 +41,6 @@ func (s *gcsBackend) Provider() string {
 }
 
 func (s *gcsBackend) SignGet(ep *url.URL, fragment pb.Fragment, d time.Duration) (string, error) {
-	log.WithFields(log.Fields{"ep": ep.String(), "fragment": fragment.ContentPath()}).Info("*** DJD1 SignGet() called")
-
 	cfg, client, opts, err := s.gcsClient(ep)
 	if err != nil {
 		return "", err
@@ -55,7 +53,6 @@ func (s *gcsBackend) SignGet(ep *url.URL, fragment pb.Fragment, d time.Duration)
 		u.Scheme = "https"
 		u.Host = "storage.googleapis.com"
 
-		log.WithFields(log.Fields{"url": u.String()}).Info("*** DJD2 SignGet()")
 		return u.String(), nil
 	} else {
 		opts.Method = "GET"
